d015f1c290c42c62e03254f2565f767c426be3e8
camilomendezp112 <camilomendezp112@gmail.com>
2026-04-29 21:48:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "infrastructure_project123456" {
+resource "aws_s3_bucket" "asset_management_infrastructure_project123456" {
   bucket = "mi-bucket-prueba-123456"
 }
