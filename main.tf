54f569f0040888387ce28abf772a1bcce3fc583e
camilomendezp112 <camilomendezp112@gmail.com>
2026-04-29 21:51:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "asset_management_infrastructure_project123456" {
-  bucket = "asset_management_infrastructure_project123456"
+resource "random_id" "bucket_id" {
+  byte_length = 4
 }
+
+resource "aws_s3_bucket" "asset_management" {
+  bucket = "camilo-${random_id.bucket_id.hex}"
+}
+
