0895a598c7f5c037d49e51d80724b88067b3118e
camilomendezp112 <camilomendezp112@gmail.com>
2026-05-01 13:29:13 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,26 @@ resource "aws_s3_bucket" "asset_management" {
   bucket = "camilo-${random_id.bucket_id.hex}"
 }
 
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 resource "aws_lambda_function" "mi_lambda" {
   function_name = "mi-lambda"
 
