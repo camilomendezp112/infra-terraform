76caecaa822a5767b90c8da6378766fa9eb51913
camilomendezp112 <camilomendezp112@gmail.com>
2026-05-01 13:20:56 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,14 @@ resource "aws_s3_bucket" "asset_management" {
   bucket = "camilo-${random_id.bucket_id.hex}"
 }
 
+resource "aws_lambda_function" "mi_lambda" {
+  function_name = "mi-lambda"
+
+  role = aws_iam_role.lambda_role.arn
+
+  handler = "index.handler"
+  runtime = "nodejs18.x"
+
+  filename = "dummy.zip"
+}
+
